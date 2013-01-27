399b38211edfc9361284fee97e3809e13ddb21ac
Trotter Cashion <cashion@gmail.com>
2013-01-27 12:32:42 -0800

    Basic dictionary

--- src/alaska/alaska.go
+++ src/alaska/alaska.go
@@ -5,5 +5,9 @@ import (
 )
 
 func main() {
-	fmt.Printf("Hello world\n")
+  dict := Dictionary{[]string{"a", "b", "c", "d"}}
+  fmt.Printf("IndexOf b: %v\n", dict.IndexOf("b"))
+  fmt.Printf("IndexOf c: %v\n", dict.IndexOf("c"))
+
+  fmt.Printf("ValueAt 2: %v\n", dict.ValueAt(2))
 }
